a1692ba21d62c10c471254cbf3c3919865f4e841
beckydvn <18rldv@queensu.ca>
2020-10-29 17:46:29 +0000

    added constraints

--- run.py
+++ run.py
@@ -1,22 +1,21 @@
 
 from nnf import Var
 from lib204 import Encoding
+from nnf import true
+from nnf import false
 
 # Call your variables whatever you want
-x = Var('x') # 🌞 
-y = Var('y') # rainy
-z = Var('z') # snow storm
-v = Var('v') # 🦠 
-d = Var('d') # document
-r = Var('r') # 🚧 
-h = Var('h') # holiday
-a = Var('a') # accident
-m = Var('m') #money
-f = Var('f') # failed to generate a trip with a reasonable time (fail model)
-c = Var('c') # 🚗 
-b = Var('b') # 🚌  
-t = Var('t') # 🚂 
-p = Var('p') # 🛩 
+sunny = Var('sunny') # 🌞 
+rainy = Var('rainy') # rainy
+snowstorm = Var('snowstorm') # snow storm
+virus = Var('virus') # 🦠 
+documents = Var('documents') # document
+roadwork = Var('roadwork') # 🚧 
+holiday = Var('holiday') # holiday
+accident = Var('accident') # accident
+money = Var('money') # money
+fail = Var('fail') # failed to generate a trip with a reasonable time (fail model)
+
 
 ottawa_start = Var('ottawa_start') # Starting in Ottawa  
 ottawa_end = Var('ottawa_end') # Ending in Ottawa
@@ -32,11 +31,64 @@ baltimore_end = Var('baltimore_end') # Ending in Baltimore
 #  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
 #  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
 #  what the expectations are.
+
+def iff(left, right):
+  return (left.negate() | right) & (right.negate() | left)
+  
 def example_theory():
     E = Encoding()
-    E.add_constraint(a | b)
-    E.add_constraint(~a | ~x)
-    E.add_constraint(c | y | z)
+    drive = Var('drive') # 🚗 
+    transit = Var('transit') # transit 
+    plane = Var('plane') # 🛩    
+    international = Var('international') # crossing the border
+    toll = Var('toll')
+     
+    #also need to add a constraint that you can only have one start and one end...
+    E.add_constraint(~toronto_start | ~(ottawa_start | scranton_start | baltimore_start))
+    E.add_constraint(~ottawa_start | ~(toronto_start | scranton_start | baltimore_start))
+    E.add_constraint(~scranton_start | ~(ottawa_start | toronto_start | baltimore_start))
+    E.add_constraint(~baltimore_start | ~(ottawa_start | scranton_start | toronto_start))
+    E.add_constraint(~toronto_end | ~(ottawa_end | scranton_end | baltimore_end))
+    E.add_constraint(~ottawa_end | ~(toronto_end | scranton_end | baltimore_end))
+    E.add_constraint(~scranton_end | ~(ottawa_end | toronto_end | baltimore_end))
+    E.add_constraint(~baltimore_end | ~(ottawa_end | scranton_end | toronto_end))
+    
+    if(((toronto_start | ottawa_start) & (scranton_end | baltimore_end)) | ((toronto_end | ottawa_end) & (scranton_start | baltimore_start))):
+      international = true
+    else:
+      international = false
+
+    #make sure weather is valid
+    E.add_constraint(iff(sunny, ~rainy))
+    E.add_constraint(iff(rainy, ~sunny))
+    E.add_constraint(iff(sunny, ~snowstorm))
+    E.add_constraint(iff(snowstorm, ~sunny))
+    
+    #good weather and holiday implies tickets will be sold out and you have to drive
+    E.add_constraint(~(sunny & holiday) | ~(transit | plane))
+    #rainy or snowstorm increases the likelihood of accidents
+    E.add_constraint(~(rainy | snowstorm) | accident)
+    #snowstorm implies that transit and planes will be shut down
+    E.add_constraint(~snowstorm | ~(transit | plane))
+    #if you have tested positive for the virus/been in contact, you can't cross the border
+    E.add_constraint(~virus | ~documents)
+    #no documents means you can't cross the border
+    E.add_constraint(~(~documents & international) | fail)
+    
+    #driving constraints (come into play if they are driving):
+    #bad weather and roadwork implies unfeasible trip
+    E.add_constraint(~(((rainy | snowstorm) & roadwork) & drive) | fail)
+    #bad weather and holiday implies unfeasible trip
+    E.add_constraint(~(((rainy | snowstorm) & holiday) & drive) | fail)
+    #roadwork and holiday implies unfeasible trip
+    E.add_constraint(~((roadwork & holiday) & drive) | fail)
+    #roadwork and accident implies unfeasible trip
+    E.add_constraint(~((roadwork & accident) & drive) | fail)
+    #holiday and accident implies unfeasible trip
+    E.add_constraint(~((holiday & accident) & drive) | fail)
+    #tolls and no money implies unfeasible trip
+    E.add_constraint(~((toll & ~money) & drive) | fail)
+
     return E
 
 
@@ -47,10 +99,12 @@ if __name__ == "__main__":
     print("\nSatisfiable: %s" % T.is_satisfiable())
     print("# Solutions: %d" % T.count_solutions())
     print("   Solution: %s" % T.solve())
-
+    """"
     print("\nVariable likelihoods:")
     for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
         print(" %s: %.2f" % (vn, T.likelihood(v)))
     print()
+    """
     
     
+
