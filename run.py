ff4a2f40e681d080dbde29296c34d4864dbe6d0b
lucasmarchd01 <71031342+lucasmarchd01@users.noreply.github.com>
2020-10-29 22:19:20 -0400

    nothing changed

--- run.py
+++ run.py
@@ -27,7 +27,8 @@ toronto_start = Var('toronto_start') # Starting in Toronto
 toronto_end = Var('toronto_end') # Ending in Toronto
 baltimore_start = Var('baltimore_start') # Starting in Baltimore
 baltimore_end = Var('baltimore_end') # Ending in Baltimore
- 
+
+ #hello
 # Build an example full theory for your setting and return it.
 #
 #  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
